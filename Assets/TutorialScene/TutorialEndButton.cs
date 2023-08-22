51c786ce1b0744eac6cb419f524041915eb79777
Mipster <tomaviv57@gmail.com>
2023-08-22 18:26:57 +0300

    done with the tutorial!

--- Assets/TutorialScene/TutorialEndButton.cs
+++ Assets/TutorialScene/TutorialEndButton.cs
@@ -5,6 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class TutorialEndButton : MonoBehaviour
 {
+    public GameObject toHide;
+    public GameObject toShow;
+    public float power = 10.0f;
+    public float radius = 5.0f;
+    public float upForce = 1.0f;
+
+    private bool buttonPressed = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,8 +26,36 @@ public class TutorialEndButton : MonoBehaviour
     }
 
 
+    IEnumerator EndTutorial() {
+        toShow.SetActive(true);
+        toHide.SetActive(false);
+        Vector3 explosionPosition = toShow.transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPosition, radius);
+
+        foreach(Collider hit in colliders)
+        {
+            hit
+                .GetComponent<Rigidbody>()
+                ?.AddExplosionForce(
+                    power,
+                    explosionPosition,
+                    radius,
+                    upForce,
+                    ForceMode.Impulse
+                );
+        }
+
+        yield return new WaitForSeconds(4);
+
+        SceneManager.LoadScene("SantaWorkshop");
+    }
+
+
     void OnTriggerEnter(Collider other)
     {
-        SceneManager.LoadScene("SantaWorkshop");
+        if(other.gameObject.tag == "Player" && !buttonPressed) {
+            buttonPressed = true;
+            StartCoroutine(EndTutorial());
+        }
     }
 }
